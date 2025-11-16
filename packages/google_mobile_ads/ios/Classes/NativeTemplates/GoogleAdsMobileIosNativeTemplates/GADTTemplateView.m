6aedb37ac48c73d4b6da3b0bd2ce4faefa6823a9
Thilo <thilo0123@gmail.com>
2025-11-16 23:27:26 +0100

    Remove border

--- packages/google_mobile_ads/ios/Classes/NativeTemplates/GoogleAdsMobileIosNativeTemplates/GADTTemplateView.m
+++ packages/google_mobile_ads/ios/Classes/NativeTemplates/GoogleAdsMobileIosNativeTemplates/GADTTemplateView.m
@@ -111,9 +111,9 @@ static NSString *const GADTBlue = @"#5C84F0";
 // Goes through all recognized style keys and updates the views accordingly,
 // overwriting the defaults.
 - (void)applyStyles {
-  self.layer.borderColor =
-      [GADTTemplateView colorFromHexString:@"E0E0E0"].CGColor;
-  self.layer.borderWidth = 1.0f;
+  // self.layer.borderColor =
+  //     [GADTTemplateView colorFromHexString:@"E0E0E0"].CGColor;
+  // self.layer.borderWidth = 1.0f;
   [self.mediaView sizeToFit];
   if ([self styleForKey:GADTNativeTemplateStyleKeyCornerRadius]) {
     float roundedCornerRadius =
